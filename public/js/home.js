b50bbcec6458842a2eb6cafb6106ea5c135fccc3
Nicholas Dimitriou <skeleynick@gmail.com>
2025-12-05 23:46:57 -0800

    reset image to crabby patty

--- public/js/home.js
+++ public/js/home.js
@@ -11,22 +11,22 @@ async function show() {
 }
 
 async function getNextRecipe() {
-    let url = "https://api.spoonacular.com/recipes/random?number=1&apiKey=d6dff3ba15ec4b89a868a2315bf77b37&includeNutrition=true";
-    let response = await fetch(url);
-    let data = await response.json();
+    // let url = "https://api.spoonacular.com/recipes/random?number=1&apiKey=d6dff3ba15ec4b89a868a2315bf77b37&includeNutrition=true";
+    // let response = await fetch(url);
+    // let data = await response.json();
 
     // BELOW IS DUMMY DATA TO BE USED WHEN NOT TESTING API (Avoid api hits cap)
-    // let data = {
-    //     recipes : [
-    //         {
-    //             "id" : 1000,
-    //             "title" : "Krabby Patty",
-    //             "image" : "/img/patty.jpg",
-    //             "summary" : "The secret formula is unknown, but the whole thing is very soggy."
-    //         }
+    let data = {
+        recipes : [
+            {
+                "id" : 1000,
+                "title" : "Krabby Patty",
+                "image" : "/img/patty.jpg",
+                "summary" : "The secret formula is unknown, but the whole thing is very soggy."
+            }
 
-    //     ]
-    // }
+        ]
+    }
     console.log(data);
 
     document.querySelector('.card-header').textContent = data.recipes[0].title;
